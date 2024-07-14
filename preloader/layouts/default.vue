28bd3675756d569a53ab484544dbfeea4a86b04c
matheusgasparx <139236157+matheusgasparx@users.noreply.github.com>
2024-07-14 01:47:52 -0300

    :100:
    Co-Authored-By: Dudu-Silva <94297173+Dudu-Silva@users.noreply.github.com>

--- preloader/layouts/default.vue
+++ preloader/layouts/default.vue
@@ -1,29 +1,24 @@
 <script setup>
 </script>
 <template>
-
-	<div id="viewport">
-		<ThePreloader />
-
-		<slot />
-	</div>
-
+  <div id="viewport">
+    <ThePreloader />
+    <slot />
+  </div>
 </template>
-<style lang='scss'>
 
-*,
-*::before,
-*::after {
-	box-sizing: border-box;
-	margin: 0;
-	padding: 0;
+<style lang="scss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+@layer base {
+  * {
+    @apply box-border m-0 p-0;
+  }
+  body {
+    @apply bg-black font-mono antialiased;
+    font-family: 'Anonymous Pro', monospace;
+  }
 }
-
-body {
-	background: #000;
-	font-family: "anonymous-pro", monospace;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-}
-
 </style>
--- preloader/nuxt.config.ts
+++ preloader/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	pages: true,
+  pages: true,
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ['@/assets/css/tailwind.css'],
+  compatibilityDate: "2024-07-14"
 })
